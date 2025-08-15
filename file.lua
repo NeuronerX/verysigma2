ab97c254267998a63853d74f082de5853f5cf24e
NeuronerX <martxjs@gmail.com>
2025-08-15 10:30:48 +0200

    Update file.lua

--- file.lua
+++ file.lua
@@ -79,8 +79,8 @@ local originalTargets = {
 -- Line formation positions (facing backward - 180 degrees turned)
 local lineTargets = {
     ["Cubot_Nova3"]     = CFrame.new(-15, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
-    ["Cub0t_01"]        = CFrame.new(5, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
-    ["cubot_nova4"]     = CFrame.new(25, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
+    ["Cub0t_01"]        = CFrame.new(3, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
+    ["cubot_nova4"]     = CFrame.new(21, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
     ["cubot_autoIoop"]  = CFrame.new(-7, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
     ["Cubot_Nova2"]     = CFrame.new(-3, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
     ["Cubot_Nova1"]     = CFrame.new(4, 125, -70, -1, 0, 0, 0, 1, 0, 0, 0, -1),
